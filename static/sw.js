01a9734f6592150bbf03841ba1ae6f7433df06e2
satyam0507@gmail.com <satyam0507@gmail.com>
2017-03-24 19:00:32 +0500

    testing async function

--- static/sw.js
+++ static/sw.js
@@ -14,23 +14,64 @@
 
 var ver = '0.0.26';
 
-// var notifyvisitors_brandid = null;
-// importScripts('./sw-notify.js');
-function fetchListener(event){
-    // console.log(request);
-    fetch(event.request).then(function(response){
-        if(response && response.status === 200){
-            return response;
-        }else{
-            console.log('not a good response');
+// // var notifyvisitors_brandid = null;
+// // importScripts('./sw-notify.js');
+// function fetchListener(event){
+//     // console.log(request);
+//     fetch(event.request).then(function(response){
+//         if(response && response.status === 200){
+//             return response;
+//         }else{
+//             console.log('not a good response');
+//         }
+//     }).catch(function(err){
+//         console.log(err);
+//     })
+// }
+
+// self.addEventListener('fetch',fetchListener);
+
+'use strict';
+
+var f1 = function () {
+  var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+    var x;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return resolveAfter2Seconds(10);
+
+          case 2:
+            x = _context.sent;
+
+            console.log(x); // 10
+
+          case 4:
+          case 'end':
+            return _context.stop();
         }
-    }).catch(function(err){
-        console.log(err);
-    })
+      }
+    }, _callee, this);
+  }));
+
+  return function f1() {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+function resolveAfter2Seconds(x) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(x);
+    }, 2000);
+  });
 }
 
-self.addEventListener('fetch',fetchListener);
-
-
+f1();
+console.log('ha');
 
 
