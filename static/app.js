002b3190af9018da6af3a02998e1525816b91e01
satyam0507@gmail.com <satyam0507@gmail.com>
2017-03-29 18:37:22 +0500

    change scope of service worker form /about to /

--- static/app.js
+++ static/app.js
@@ -15,13 +15,20 @@
 console.log('app.js');
 
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('sw.js', { scope: '/about' }).then(function () {
+    navigator.serviceWorker.register('sw.js', { scope: '/' }).then(function () {
         console.log('service worker registered successfully 1');
         Notification.requestPermission(function (result) {
             if (result === 'granted') {
                 console.log('premission granted');
             }
         })
+        sendMessage('this is your client')
+            .then(e => {
+                console.log(e);
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }).catch(function (err) {
         console.log('not able to register service worker :: ' + err);
     })
@@ -36,26 +43,34 @@ if (navigator.serviceWorker) {
     //     console.log('not able to register service worker :: ' + err);
     // })
 }
-
-(function () {
-    var btn = document.getElementById('btn');
-    btn.addEventListener('click', function (evt) {
-        // evt.preventDefault();
-        // var data={
-        //     name:'satyam singh',
-        //     data:'haha'
-        // }
-        fetch('/data', {
-            method: "POST",
-            headers: new Headers({
-                'Content-Type': 'application/json'
-            })
-            // ,
-            // body: JSON.stringify(data)
-        }).then(function (data) {
-            console.log(data);
-        }).catch(function (err) {
-            console.log(err);
-        })
-    })
-})()
+function sendMessage(message) {
+    return new Promise(function (resolve, reject) {
+        // note that this is the ServiceWorker.postMessage version
+        navigator.serviceWorker.controller.postMessage(message);
+        navigator.serviceWorker.onMessage = function (e) {
+            resolve(e);
+        };
+    });
+}
+// (function () {
+//     var btn = document.getElementById('btn');
+//     btn.addEventListener('click', function (evt) {
+//         // evt.preventDefault();
+//         // var data={
+//         //     name:'satyam singh',
+//         //     data:'haha'
+//         // }
+//         fetch('/data', {
+//             method: "POST",
+//             headers: new Headers({
+//                 'Content-Type': 'application/json'
+//             })
+//             // ,
+//             // body: JSON.stringify(data)
+//         }).then(function (data) {
+//             console.log(data);
+//         }).catch(function (err) {
+//             console.log(err);
+//         })
+//     })
+// })()
